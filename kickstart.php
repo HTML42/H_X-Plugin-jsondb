e6cbe8f87c78b8150ba44b29c06969cf38ddfcf7
Mark Paspirgilis <service@html42.de>
2018-12-25 13:58:23 +0100

    init jsondb

--- kickstart.php
+++ kickstart.php
@@ -5,8 +5,8 @@ define('X_JSONDB_PATH', PROJECT_ROOT . 'plugins/jsondb/');
 
 #Files
 foreach (array('classes/xjsondb.class.php') as $plugin_file_path) {
-    if (is_file(X_LOGIN_PATH . $plugin_file_path)) {
-        include X_LOGIN_PATH . $plugin_file_path;
+    if (is_file(X_JSONDB_PATH . $plugin_file_path)) {
+        include X_JSONDB_PATH . $plugin_file_path;
     }
 }
 
